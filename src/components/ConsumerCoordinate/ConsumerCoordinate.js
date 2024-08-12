8e97430bdd53ebef329b0b12c465ae77aecff368
protsyshyn_taras <protsyshyn.t@gmail.com>
2024-08-12 10:42:32 +0300

    make it usable in mobile

--- src/components/ConsumerCoordinate/ConsumerCoordinate.js
+++ src/components/ConsumerCoordinate/ConsumerCoordinate.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
+import Hamburger from "hamburger-react";
 import { getWGSDecimalFromText } from "./parseText";
 
 import s from "./ConsumerCoordinate.module.css";
-import { useState } from "react";
 
 export const ConsumerCoordinate = ({ className, onParse }) => {
+  const [isOpen, setOpen] = useState(true);
   const [text, setText] = useState("");
 
   const onChange = (e) => {
@@ -15,10 +17,17 @@ export const ConsumerCoordinate = ({ className, onParse }) => {
   };
 
   return (
-    <div className={`${s.wrapper} ${className}`}>
-      <h1 className={s.title}>Текст з координатами </h1>
+    <div className={`${s.wrapper} ${isOpen ? "" : s.wrapperClose} ${className}`}>
+      <div className={s.head}>
+        <h1 className={s.title}>Текст з координатами </h1>
+        <div className={s.burger}>
+          <Hamburger toggled={isOpen} toggle={setOpen} size={24} />
+        </div>
+      </div>
       <textarea onChange={onChange} input={text} className={s.input} rows={5} />
-      <button onClick={parse}>Відобразити на карті</button>
+      <button className={s.btn} onClick={parse}>
+        Відобразити на карті
+      </button>
     </div>
   );
 };
