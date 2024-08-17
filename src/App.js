4777ce77a0b4476a45af6c5529dfad0ace85af74
protsyshyn_taras <protsyshyn.t@gmail.com>
2024-08-17 12:00:37 +0300

    save a cords in search params, dislay them as a list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { ConsumerCoordinate, Map } from "./components";
+import { useCordsFromSearch } from "./hooks";
 
 import s from "./App.module.css";
 
 function App() {
-  const [coordinate, setCoordinate] = useState([]);
+  const [cords, setCoordinate] = useCordsFromSearch();
+  const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    if (map && cords.length > 0) {
+      map.setView(cords[0], 9);
+    }
+  }, [map, cords]);
+
+  const onParse = (cords) => {
+    setCoordinate(cords);
+  };
+
+  const onRemove = (el) => {
+    setCoordinate(cords.filter(([lat, lon]) => !(el[0] === lat && el[1] === lon)));
+  };
 
   return (
     <div className={s.app}>
-      <ConsumerCoordinate className={s.coordinate} onParse={setCoordinate} />
-      <Map coordinate={coordinate} />
+      <ConsumerCoordinate
+        className={s.coordinate}
+        onSetView={(cord) => map.setView(cord, 12)}
+        onParse={onParse}
+        cords={cords}
+        onRemove={onRemove}
+      />
+      <Map setMap={setMap} coordinate={cords} />
     </div>
   );
 }
--- src/components/ConsumerCoordinate/ConsumerCoordinate.js
+++ src/components/ConsumerCoordinate/ConsumerCoordinate.js
@@ -4,7 +4,7 @@ import { getWGSDecimalFromText } from "./parseText";
 
 import s from "./ConsumerCoordinate.module.css";
 
-export const ConsumerCoordinate = ({ className, onParse }) => {
+export const ConsumerCoordinate = ({ className, onParse, cords, onSetView, onRemove }) => {
   const [isOpen, setOpen] = useState(true);
   const [text, setText] = useState("");
 
@@ -16,18 +16,46 @@ export const ConsumerCoordinate = ({ className, onParse }) => {
     onParse(getWGSDecimalFromText(text));
   };
 
+  const clear = () => {
+    onParse([]);
+  };
+
   return (
     <div className={`${s.wrapper} ${isOpen ? "" : s.wrapperClose} ${className}`}>
       <div className={s.head}>
-        <h1 className={s.title}>Текст з координатами </h1>
+        <div className={s.subHead}>
+          <h1 className={s.title}>Текст з координатами </h1>
+          <p className={s.subTitle}>Координати в форматі WGS-84 десяткові</p>
+        </div>
         <div className={s.burger}>
           <Hamburger toggled={isOpen} toggle={setOpen} size={24} />
         </div>
       </div>
-      <textarea onChange={onChange} input={text} className={s.input} rows={5} />
-      <button className={s.btn} onClick={parse}>
-        Відобразити на карті
-      </button>
+      {cords.length ? (
+        <ul className={s.cordsList}>
+          {cords.map((el) => (
+            <li className={s.cordsListItem} key={el[0] + "-" + el[1]}>
+              <button onClick={() => onSetView(el)} className={s.cord}>
+                {el[0]}; {el[1]}
+              </button>
+              <button onClick={() => onRemove(el)} className={s.remove}>
+                x
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <textarea onChange={onChange} input={text} className={s.input} rows={5} />
+      )}
+      {cords.length ? (
+        <button className={s.btn} onClick={clear}>
+          Очистити
+        </button>
+      ) : (
+        <button className={s.btn} onClick={parse}>
+          Відобразити на карті
+        </button>
+      )}
     </div>
   );
 };
